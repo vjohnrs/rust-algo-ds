303082b0607d7a26b0182cd05d37dc3175dc0426
vjohn <vinothj23@gmail.com>
2024-09-22 14:18:36 -0400

    corrected heap sort algorithm

--- src/data_structures/heap.rs
+++ src/data_structures/heap.rs
@@ -43,7 +43,6 @@ impl Heap {
         }
     }
     
-
     fn swap (&mut self, i: usize, j: usize){
         let mut arr = &mut self.arr;
         let mut temp = arr[i];
@@ -68,72 +67,73 @@ impl Heap {
     pub fn insert (&mut self, val: i64) {
         self.arr.push(val);
     }  
-  // Todo
-  /* pub fn heap_sort (&mut self) { 
-        
-        self.build_max_heap();        
+
+    pub fn heap_sort (&mut self) -> &Vec<i64> { 
+        self.build_max_heap();  
+
         for i in (1..self.arr.len()).rev(){
             self.swap(0, i);
             self.heapsize = self.heapsize - 1;
-            self.max_heapify(1);
+            self.max_heapify(0);
         }
-    } */ 
+
+        return &self.arr;
+    }  
 }
 
- #[cfg(test)]
- mod tests { 
-     use super::*; 
-     #[test] 
-fn invoke(){        
-}
+#[cfg(test)]
+mod tests { 
+    use super::*; 
 
-#[test]    
-fn test_parent_child_for_0(){
-    assert_eq!(Heap::parent(1), 0);
-    assert_eq!(Heap::parent(2), 0);
-    assert_ne!(Heap::parent(3), 0);
-    assert_ne!(Heap::parent(4), 0);
-    assert_eq!(Heap::left(0), 1);
-    assert_eq!(Heap::right(0), 2);    
-}
+    #[test] 
+    fn invoke(){        
+    }
 
-#[test]
-fn test_partent_child_for_1(){
-    assert_eq!(Heap::left(1), 3);
-    assert_eq!(Heap::right(1), 4);
-    assert_eq!(Heap::parent(3), 1);
-    assert_eq!(Heap::parent(4), 1);
-}
+    #[test]    
+    fn test_parent_child_for_0(){
+        assert_eq!(Heap::parent(1), 0);
+        assert_eq!(Heap::parent(2), 0);
+        assert_ne!(Heap::parent(3), 0);
+        assert_ne!(Heap::parent(4), 0);
+        assert_eq!(Heap::left(0), 1);
+        assert_eq!(Heap::right(0), 2);    
+    }
+
+    #[test]
+    fn test_partent_child_for_1(){
+        assert_eq!(Heap::left(1), 3);
+        assert_eq!(Heap::right(1), 4);
+        assert_eq!(Heap::parent(3), 1);
+        assert_eq!(Heap::parent(4), 1);
+    }
 
 
-#[test]
-fn max_heapify_at_index_0(){
-    let mut heap = Heap::new(vec![1,2,3]);
-    heap.max_heapify(0);
-    assert_eq!(heap.get_arr(), &vec![3,2,1]);
-}
+    #[test]
+    fn max_heapify_at_index_0(){
+        let mut heap = Heap::new(vec![1,2,3]);
+        heap.max_heapify(0);
+        assert_eq!(heap.get_arr(), &vec![3,2,1]);
+    }
 
-#[test]
-fn max_heapify_at_index_1(){
-    let mut heap = Heap::new(vec![16,4,10,14,7,9,3,2,8,1]);
-    heap.max_heapify(1);
-    assert_eq!(heap.get_arr(), &vec![16,14,10,8,7,9,3,2,4,1]);
-}
-#[test]
-fn build_max_heap(){
-    let mut heap = Heap::new(vec![4,1,3,2,16,9,10,14,8,7]);
-    heap.build_max_heap();
-    println!("{:?}", heap.get_arr());    
-}
- // Todo
- /* 
-#[test]
-fn run_heap_sort(){
-    let mut heap = Heap::new(vec![4,1,3,2,16,9,10,14,8,7]);
-    heap.heap_sort();
-    println!("sorted data {:?}", heap.get_arr());    
-}
-*/
+    #[test]
+    fn max_heapify_at_index_1(){
+        let mut heap = Heap::new(vec![16,4,10,14,7,9,3,2,8,1]);
+        heap.max_heapify(1);
+        assert_eq!(heap.get_arr(), &vec![16,14,10,8,7,9,3,2,4,1]);
+    }
+    #[test]
+    fn build_max_heap(){
+        let mut heap = Heap::new(vec![4,1,3,2,16,9,10,14,8,7]);
+        heap.build_max_heap();
+        println!("{:?}", heap.get_arr());    
+    }
+    // Todo
+    
+    #[test]
+    fn run_heap_sort(){
+        let mut heap = Heap::new(vec![4,1,3,2,16,9,10,14,8,7]);
+        assert_eq!(heap.heap_sort(), &vec![1, 2, 3, 4, 7, 8, 9, 10, 14, 16]);
+    }
 
 }
 
