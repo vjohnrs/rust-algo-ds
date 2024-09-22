6fb51adcf15aee74c43ce73589067f460ff7679f
vjohn <vinothj23@gmail.com>
2024-09-22 16:02:44 -0400

    rewrite from memory with simplifications

--- src/data_structures/heap.rs
+++ src/data_structures/heap.rs
@@ -3,83 +3,78 @@ pub struct Heap {
     heapsize: usize
 }
 
-impl Heap {    
-    fn parent(i: usize) -> usize {
-        (i - 1) / 2
-    }
-    
-    fn left(i: usize) -> usize {
-        (2 * i) + 1
+impl Heap {
+    fn parent (i: usize) -> usize {
+        (i - 1)/ 2
     }
 
-    fn right(i: usize) -> usize {
+    fn left (i: usize) -> usize {
+        (2 * i) + 1      
+    }
+
+    fn right (i: usize) -> usize {
         (2 * i) + 2
     }
 
-    fn get_arr (&self) -> &Vec<i64> {
-        return &self.arr;
-    }
-
-    fn max_heapify(&mut self, i: usize){
-
-        let mut arr = &mut self.arr;
-
-        let l = Self::left(i);
-        let r = Self::right(i);
-
-        let mut largest:usize = i;
-
-        if l < self.heapsize && arr[l] > arr[largest] {
-            largest = l; 
-        } 
+    fn new(arr: Vec<i64>) -> Self {
         
-        if r < self.heapsize && arr[r] > arr[largest] {
-            largest = r; 
-        } 
+        let len = arr.len();
 
-        if largest != i {
-            self.swap(i, largest);            
-            self.max_heapify(largest);
-        }
-    }
-    
-    fn swap (&mut self, i: usize, j: usize){
-        let mut arr = &mut self.arr;
-        let mut temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    fn build_max_heap(&mut self) {                        
-        for i in (0..(&self.arr.len() / 2)).rev() {
-            self.max_heapify(i);
-        }
-    }
-    
-    pub fn new (input_arr: Vec<i64>) -> Self {
-        let len = input_arr.len();
-        return Heap {
-            arr: input_arr,
+        return Self {
+            arr: arr,
             heapsize: len
         }
     }
 
-    pub fn insert (&mut self, val: i64) {
-        self.arr.push(val);
-    }  
+    fn swap (&mut self, i:usize, j:usize) {
+        let mut arr = &mut self.arr;
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
-    pub fn heap_sort (&mut self) -> &Vec<i64> { 
-        self.build_max_heap();  
+    fn max_heapify (&mut self, i:usize) {
 
-        for i in (1..self.arr.len()).rev(){
+        let mut arr = &mut self.arr;
+
+        let mut largest = i; 
+        let left = Self::left(i);
+        let right = Self::right(i);
+
+        if left < self.heapsize && arr[left] >= arr[largest] {
+            largest = left; 
+        }
+
+        if right < self.heapsize && arr[right] >= arr[largest] {
+            largest = right; 
+        }
+
+        if i != largest {
+            self.swap(i, largest);
+            self.max_heapify(largest);
+        }
+    }
+
+    fn build_max_heap (&mut self) {
+        for i in (0..(&self.arr.len() / 2)).rev() {
+            &self.max_heapify(i);
+        }
+    }
+
+    fn heap_sort (&mut self) {
+                
+        self.build_max_heap();
+
+        for i in (0..self.arr.len()).rev(){       
             self.swap(0, i);
-            self.heapsize = self.heapsize - 1;
+            self.heapsize -= 1;
             self.max_heapify(0);
         }
 
-        return &self.arr;
-    }  
+    }
 }
+
+
 
 #[cfg(test)]
 mod tests { 
@@ -112,27 +107,28 @@ mod tests {
     fn max_heapify_at_index_0(){
         let mut heap = Heap::new(vec![1,2,3]);
         heap.max_heapify(0);
-        assert_eq!(heap.get_arr(), &vec![3,2,1]);
+        assert_eq!(heap.arr, vec![3,2,1]);
     }
 
     #[test]
     fn max_heapify_at_index_1(){
         let mut heap = Heap::new(vec![16,4,10,14,7,9,3,2,8,1]);
         heap.max_heapify(1);
-        assert_eq!(heap.get_arr(), &vec![16,14,10,8,7,9,3,2,4,1]);
+        assert_eq!(heap.arr, vec![16,14,10,8,7,9,3,2,4,1]);
     }
+
     #[test]
     fn build_max_heap(){
         let mut heap = Heap::new(vec![4,1,3,2,16,9,10,14,8,7]);
         heap.build_max_heap();
-        println!("{:?}", heap.get_arr());    
+        assert_eq!(heap.arr, vec![16, 14, 10, 8, 7, 9, 3, 2, 4, 1]);        
     }
-    // Todo
-    
+
     #[test]
     fn run_heap_sort(){
         let mut heap = Heap::new(vec![4,1,3,2,16,9,10,14,8,7]);
-        assert_eq!(heap.heap_sort(), &vec![1, 2, 3, 4, 7, 8, 9, 10, 14, 16]);
+        heap.heap_sort();
+        assert_eq!(heap.arr, vec![1, 2, 3, 4, 7, 8, 9, 10, 14, 16]);
     }
 
 }
